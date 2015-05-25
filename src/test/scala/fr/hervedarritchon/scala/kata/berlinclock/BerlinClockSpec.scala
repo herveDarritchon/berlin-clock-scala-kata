35da4c25aeed62905e6968d827f0e19e7e9ac288
Hervé Darritchon <herve.darritchon@gmail.com>
2015-05-25 20:53:26 +0200

    Evolution: Add some tests for minutes.

--- src/test/scala/fr/hervedarritchon/scala/kata/berlinclock/BerlinClockSpec.scala
+++ src/test/scala/fr/hervedarritchon/scala/kata/berlinclock/BerlinClockSpec.scala
@@ -14,32 +14,47 @@ class BerlinClockSpec extends FeatureSpec with GivenWhenThen with Matchers {
   feature("As a clock user I can accurately tell the time down to the minute") {
     scenario("It's twelve o'clock so no lamp from the bottom line is illuminated") {
       Given("I have started the converter")
+      BerlinClock.start()
       When("I enter Twelve o'clock")
+      val berlinClock: Array[Array[String]] = BerlinClock.convert(LocalTime.of(12, 0, 0))
       Then("No lamp is illuminated")
+      berlinClock should be("OOOO")
     }
 
     scenario("It's twelve past 2mn so the first two lamps from the bottom line are illuminated") {
       Given("I have started the converter")
+      BerlinClock.start()
       When("I enter Twelve o'clock")
+      val berlinClock: Array[Array[String]] = BerlinClock.convert(LocalTime.of(12, 2, 0))
       Then("No lamp is illuminated")
+      berlinClock should be("YYOO")
     }
 
     scenario("It's twelve past 5mn. So only the first lamp from the second line is illuminated") {
       Given("I have started the converter")
+      BerlinClock.start()
       When("I enter Twelve o'clock")
+      val berlinClock: Array[Array[String]] = BerlinClock.convert(LocalTime.of(12, 0, 0))
       Then("No lamp is illuminated")
+      berlinClock should be("OOOO")
     }
 
     scenario("It's twelve past 15mn. So the first 3 lamps from the second line are illuminated and the third is red") {
       Given("I have started the converter")
+      BerlinClock.start()
       When("I enter Twelve o'clock")
+      val berlinClock: Array[Array[String]] = BerlinClock.convert(LocalTime.of(12, 15, 0))
       Then("No lamp is illuminated")
+      berlinClock should be("OOOO")
     }
 
     scenario("It's twelve past 45mn. So the first 9 lamps from the second line are illuminated and the third, the sixth and the ninth are illuminated in red") {
       Given("I have started the converter")
+      BerlinClock.start()
       When("I enter Twelve o'clock")
+      val berlinClock: Array[Array[String]] = BerlinClock.convert(LocalTime.of(12, 45, 0))
       Then("No lamp is illuminated")
+      berlinClock should be("OOOO")
     }
 
   }
