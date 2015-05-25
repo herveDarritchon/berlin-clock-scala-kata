c2714fc41ff12cca1595c13d35ca6d6ba1aa7773
Hervé Darritchon <herve.darritchon@gmail.com>
2015-05-25 10:04:50 +0200

    Evolution: Change tests from FlatSpec to FeatureSpec.

--- src/test/scala/fr/hervedarritchon/scala/kata/berlinclock/BerlinClockSpec.scala
+++ src/test/scala/fr/hervedarritchon/scala/kata/berlinclock/BerlinClockSpec.scala
@@ -1,15 +1,17 @@
 package fr.hervedarritchon.scala.kata.berlinclock
 
-import org.scalatest.{FlatSpec, Matchers}
+import org.scalatest.FeatureSpec
 
 /**
  * Created by Hervé Darritchon on 23/05/15.
  *
  */
-class BerlinClockSpec extends FlatSpec with Matchers {
+class BerlinClockSpec extends FeatureSpec {
 
-  "BerlinClockSpec" should "have tests" in {
-    true should ===(true)
+  feature("A user can do whatever the test need to be") {
+    scenario("the use can do one thing")(pending)
+
+    scenario("the use can do a second thing")(pending)
   }
 
 }
